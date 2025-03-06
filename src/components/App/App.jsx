ffa24415876a2ffdf52890b92028b8907855ca0a
yuliia_mrachynska <mrachynska.y@gmail.com>
2025-03-06 14:58:39 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Description from '../Description/Description'
 import Options from '../Options/Options'
 import Feedback from '../Feedback/Feedback'
-// import './App.css'
+import Notification from '../Notification/Notification'
+import css from './App.module.css'
 
 function App() {
   const [feedback, setFeedback] = useState({
@@ -11,28 +12,48 @@ function App() {
     bad: 0
   })
 
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('feedback'))
+    if (saved) {
+      setFeedback(saved)
+    }
+}, [])
+
+  useEffect(() => {
+    if (feedback) {
+localStorage.setItem('feedback', JSON.stringify(feedback))
+    }
+}, [feedback])
+
   const updateFeedback = (feedbackType) => { 
   setFeedback({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 })
   }
   
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const resetFeedback = () => {
-    setFeedback({good: 0, neutral: 0, bad: 0})
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+    localStorage.removeItem('feedback')
   }
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100)
+
 
 
   return (
-    <>
+    <div className={css.container}>
       <Description />
       <Options
         update={updateFeedback}
         total={totalFeedback}
         reset={resetFeedback}
       />
-      <Feedback
-        feedbacks={feedback}
-      />
-    </>
+      {totalFeedback === 0 ?( <Notification />) :
+      (<Feedback
+          feedbacks={feedback}
+          total={totalFeedback}
+          positive={positiveFeedback}
+      />)
+  }
+    </div>
   )
 }
 
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,11 +1,12 @@
+import css from './Options.module.css'
 
 export default function Options({update, total, reset}) {
     return (
         <div>
-            <button onClick={() => update("good")}>Good</button>
-            <button onClick={() => update("neutral")}>Neutral</button>
-            <button onClick={() => update("bad")}>Bad</button>
-            {total === 0 || <button onClick={reset}>Reset</button>}
+            <button className={css.btn} onClick={() => update("good")}>Good</button>
+            <button className={css.btn} onClick={() => update("neutral")}>Neutral</button>
+            <button className={css.btn} onClick={() => update("bad")}>Bad</button>
+            {total === 0 || <button className={css.btn} onClick={reset}>Reset</button>}
     </div>
 )
 }
